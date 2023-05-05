0275bafee7d7ecc8a5f3d78402e9990589a4a986
bilgee0119 <bilgeeen416@gmail.com>
2023-05-05 11:36:33 +0800

    05/05

--- music/App.js
+++ music/App.js
@@ -5,14 +5,32 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreens from './screens/HomeScreens';
 import MusicScreen from './screens/MusicScreen';
+import LikeScreen from './screens/LikeScreen';
 
 export default function App() {
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={HomeScreens}/>
-        <Tab.Screen name='Noot' component={MusicScreen}/>
+      <Tab.Navigator screenOptions={({route}) => ({
+        tabBarIcon: ({focused, size, colour}) =>{
+          let iconName;
+          if (route.name === 'Нүүр'){
+            iconName = focused ? 'ios-home':'ios-home-outline';
+          } 
+          else if (route.name === 'Дуу'){
+            iconName = focused ? 'musical-notes':'musical-notes-outline';
+          }
+          else if (route.name === 'Таалагдсан'){
+            iconName = focused ? 'heart':'heart-outline';
+          }
+          return <Ionic name={iconName} size={size} colour={colour} />;
+        },
+        
+      }
+      )}>
+        <Tab.Screen name='Нүүр' component={HomeScreens}/>
+        <Tab.Screen name='Дуу' component={MusicScreen}/>
+        <Tab.Screen name='Таалагдсан' component={LikeScreen}/>
       </Tab.Navigator>
     </NavigationContainer>
   )
